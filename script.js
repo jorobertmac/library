1b0e9ebb4ecb44eb5f72ec9b9c04a027b6780144
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-08-05 14:52:11 -0700

    Add reading progress attribut to book object

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myLibrary = {}
 const cards = document.querySelector("#cards")
 
-function Book(title, author, pages, description = "", read=false, artSource=undefined) {
+function Book(title, author, pages, progress=0, description = "", read=false, artSource=undefined) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.progress = progress
   this.description = description
   this.read = read
   this.artSource = artSource
@@ -86,7 +87,7 @@ function makeCard(book) {
 
   const pages = document.createElement("p")
   pages.className = "pages"
-  pages.textContent = `${book.pages} pages`
+  pages.textContent = `${book.progress} / ${book.pages} pages`
   card.appendChild(pages)
   
   const description = document.createElement("p")
@@ -119,15 +120,15 @@ function makeCard(book) {
   setRemoveBookButton(book)
 }
 
-addBookToLibrary(new Book("Whispers of the Void", "Elara Kade", 342, "A thrilling sci-fi adventure following a rogue pilot uncovering a galaxy-wide conspiracy.", true, "./art/whispers_of_the_void_1.jpg"))
-addBookToLibrary(new Book("The Crimson Veil", "Torin Vell", 487, "A gothic romance where a young scholar unravels the secrets of a haunted manor.", true, "./art/crimson_veil_2.jpg"))
-addBookToLibrary(new Book("Echoes Over Emberfall", "Sylas Wren", 256, "A fantasy tale of a cursed bard seeking redemption in a war-torn kingdom.", false, "./art/echos_over_emberfall_1.jpg"))
-addBookToLibrary(new Book("Glass Horizons", "Mira Thalor", 613, "A dystopian novel about a hacker navigating a city of transparent walls and hidden truths.", true, "./art/glass_horizons_1.jpg"))
-addBookToLibrary(new Book("The Starwoven Tapestry", "Kael Draven", 391, "An epic saga of a weaver who binds the fates of gods and mortals.", true, "./art/starwoven_tapestry_1.jpg"))
-addBookToLibrary(new Book("Shades of Iron", "Vera Quill", 178, "A gritty western where a lone gunslinger confronts her past in a lawless frontier.", false, "./art/shade_of_iron_2.jpg"))
-addBookToLibrary(new Book("The Last Clockmaker", "Oren Feld", 524, "A steampunk mystery about a clockmaker racing to stop a time-altering catastrophe.", true, "./art/last_clockmaker_1.jpg"))
-addBookToLibrary(new Book("Tides of Forgotten Songs", "Lirien Voss", 299, "A lyrical journey of a mermaid bard rediscovering her lost heritage.", false, "./art/tides_of_forgotten_songs_1.jpg"))
-addBookToLibrary(new Book("The Obsidian Crown", "Drenar Holt", 465, "A dark fantasy where a exiled prince battles to reclaim a cursed throne.", false, "./art/obsidian_crown_2.jpg"))
-addBookToLibrary(new Book("Silent Spires", "Auren Zeth", 320, "A post-apocalyptic tale of a scavenger exploring ancient towers for forgotten tech.", false, "./art/silent_spires_2.jpg"))
+addBookToLibrary(new Book("Whispers of the Void", "Elara Kade", 342, 111, "A thrilling sci-fi adventure following a rogue pilot uncovering a galaxy-wide conspiracy.", true, "./art/whispers_of_the_void_1.jpg"))
+addBookToLibrary(new Book("The Crimson Veil", "Torin Vell", 487, 111, "A gothic romance where a young scholar unravels the secrets of a haunted manor.", true, "./art/crimson_veil_2.jpg"))
+addBookToLibrary(new Book("Echoes Over Emberfall", "Sylas Wren", 256, 0, "A fantasy tale of a cursed bard seeking redemption in a war-torn kingdom.", false, "./art/echos_over_emberfall_1.jpg"))
+addBookToLibrary(new Book("Glass Horizons", "Mira Thalor", 613, 111, "A dystopian novel about a hacker navigating a city of transparent walls and hidden truths.", true, "./art/glass_horizons_1.jpg"))
+addBookToLibrary(new Book("The Starwoven Tapestry", "Kael Draven", 391, 111, "An epic saga of a weaver who binds the fates of gods and mortals.", true, "./art/starwoven_tapestry_1.jpg"))
+addBookToLibrary(new Book("Shades of Iron", "Vera Quill", 178, 0, "A gritty western where a lone gunslinger confronts her past in a lawless frontier.", false, "./art/shade_of_iron_2.jpg"))
+addBookToLibrary(new Book("The Last Clockmaker", "Oren Feld", 524, 111, "A steampunk mystery about a clockmaker racing to stop a time-altering catastrophe.", true, "./art/last_clockmaker_1.jpg"))
+addBookToLibrary(new Book("Tides of Forgotten Songs", "Lirien Voss", 299, 0, "A lyrical journey of a mermaid bard rediscovering her lost heritage.", false, "./art/tides_of_forgotten_songs_1.jpg"))
+addBookToLibrary(new Book("The Obsidian Crown", "Drenar Holt", 465, 0, "A dark fantasy where a exiled prince battles to reclaim a cursed throne.", false, "./art/obsidian_crown_2.jpg"))
+addBookToLibrary(new Book("Silent Spires", "Auren Zeth", 320, 0, "A post-apocalyptic tale of a scavenger exploring ancient towers for forgotten tech.", false, "./art/silent_spires_2.jpg"))
 
 displayBooks()
